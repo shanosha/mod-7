6a78ab1ea42853184651f5de7c5ce349d99f4f1c
Shanosha <wootenm1@gmail.com>
2026-02-02 20:40:47 -0500

    Update the location data displayed to the user on page load

--- src/main.js
+++ src/main.js
@@ -4,11 +4,25 @@ import "leaflet/dist/leaflet.css";
 import { fetchGeoLocation } from "./services/geoApiService.js";
 
 
-const geoLocationData = await fetchGeoLocation();
-console.log(geoLocationData);
-const lat = geoLocationData.location.lat;// 40.57927
-const lng = geoLocationData.location.lng;// -74.41154
+const ip = document.getElementById("ip");
+const location = document.getElementById("location");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("isp");
+const form = document.getElementById("searchForm");
+const searchInput = document.getElementById("search");
 
+const data = await fetchGeoLocation();
+console.log(data);
+
+// Update the location data displayed to the user on page load
+ip.textContent = data.ip;
+location.textContent = data.location.city;
+timezone.textContent = data.location.timezone;
+isp.textContent = data.isp;
+
+// Update the leaflet map location and marker
+const lat = data.location.lat;// 40.57927
+const lng = data.location.lng;// -74.41154
 const map = L.map("map").setView([lat, lng], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -16,3 +30,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 var marker = L.marker([lat, lng]).addTo(map);
 marker.bindPopup("<b>Current Location</b><br>This is where you are.");//.openPopup()
+
+// Event listeners
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("click");
+
+    if(searchInput.value != ""){
+
+    }
+});
