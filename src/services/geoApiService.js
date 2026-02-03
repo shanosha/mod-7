e19665903009c26a3028d986b1b777fc9c06bd54
Shanosha <wootenm1@gmail.com>
2026-02-02 19:47:37 -0500

    created mock data so API isn't used during development

--- src/services/geoApiService.js
+++ src/services/geoApiService.js
@@ -1,9 +1,11 @@
 import { key } from "../../secret.js";
 import { ApiError, handleError } from "../utils/errorHandler.js";
+import mockData from "../utils/mockData.js";
 
 async function getPublicIP() {
   try {
     const response = await fetch("https://api.ipify.org?format=json");
+    
     const data = await response.json();
     const ipAddress = data.ip;
 
@@ -20,24 +22,13 @@ async function fetchGeoLocation() {
   try {
     const ipAddress = await getPublicIP() || "8.8.8.8";
     const apiKey = key; // Set this value to your API key
-    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
-
-    console.log("API Key", apiKey);
-    console.log("URL", url);
-
-    const response = await fetch(url); //?limit=10
-
-    if (!response.ok) {
-      throw new ApiError("API request failed", response.status);
-    }
-
-    const data = await response.json();
-    console.log(data);
-
-    // const products = data.products.map(
-    //     (p) => new Product(p.id, p.title, p.category, p.price, p.discountPercentage)
-    // );
-
+    // const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+    // const response = await fetch(url); //?limit=10
+    // if (!response.ok) {
+    //   throw new ApiError("API request failed", response.status);
+    // }
+    // const data = await response.json();
+    const data = mockData;
     return data;
   } catch (e) {
     handleError(e);
---
+++ src/utils/mockData.js
@@ -0,0 +1,21 @@
+export default {
+    "ip": "24.0.24.106",
+    "location": {
+        "country": "US",
+        "region": "New Jersey",
+        "city": "South Plainfield",
+        "lat": 40.57927,
+        "lng": -74.41154,
+        "postalCode": "07080",
+        "timezone": "-05:00",
+        "geonameId": 5104844
+    },
+    "as": {
+        "asn": 7922,
+        "name": "COMCAST-7922",
+        "route": "24.0.0.0\/12",
+        "domain": "https:\/\/corporate.comcast.com\/",
+        "type": "Cable\/DSL\/ISP"
+    },
+    "isp": "Comcast"
+}
