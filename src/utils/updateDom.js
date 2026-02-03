3623972a297bcd441c682f7db8313c2d56e69f64
Shanosha <wootenm1@gmail.com>
2026-02-03 13:30:51 -0500

    feature/added comments to updatDom module

--- src/utils/updateDom.js
+++ src/utils/updateDom.js
@@ -13,19 +13,19 @@ function renderLocationData(data){
 
 }
 
+// Check input to see if the pattern matches a URL or domain format
 function getHostType(input) {
+
   const value = input.trim();
 
-  const ipRegex =
-    /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/;
-
-  const domainRegex =
-    /^(?!-)(?:[a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,}$/;
+  const ipRegex = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}$/;
+  const domainRegex = /^(?!-)(?:[a-zA-Z0-9-]{1,63}\.)+[a-zA-Z]{2,}$/;
 
   if (ipRegex.test(value)) return "ip";
   if (domainRegex.test(value)) return "domain";
-
+  
   return "invalid";
+
 }
 
 export { renderLocationData, getHostType}
